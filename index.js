14a41b4cc58601ea080aa7995f4c2b8028984f65
Darrell Klein <darrelldklein@gmail.com>
2018-09-18 13:37:59 -0700

    initial commit, 1.5hrs of work during downtime at Novation

---
+++ index.js
@@ -0,0 +1,63 @@
+const contacts = [
+    {
+        name: 'Jimmie',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie3',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie4',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie5',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie6',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie7',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    },
+    {
+        name: 'Jimmie9',
+        email: 'jimmie@gmail.com',
+        phone: '555-555-5555',
+        address: '123 S. Somewhere'
+    }
+];
+
+const contactList = document.getElementById('contacts-list');
+const contactslen = contacts.length;
+
+const addContacts = () => {
+    contacts.forEach(function(contact){
+        let newContact = document.createElement('div');
+        newContact.className = 'contacts-list--item';
+        contactList.appendChild(newContact);
+        let newName = document.createElement('div');
+        newName.classList = 'contact-name';
+        newName.textContent = contact.name;
+        newContact.appendChild(newName);
+        let newInfo = document.createElement('div');
+        newInfo.classList = 'contact-info';
+        newInfo.textContent = contact.email;
+        newContact.appendChild(newInfo);       
+    }, contacts)
+}
